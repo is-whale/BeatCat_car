a52f17a5fb36cdf4596e597f9e765a4979bf5bc4
is-whale <2966365639@qq.com>
2024-01-12 18:06:11 +0800

    add USE_STAMPED ,but is't be test

--- best_planner/vehiclecmd2twist/src/vehiclecmd2twist_node.cpp
+++ best_planner/vehiclecmd2twist/src/vehiclecmd2twist_node.cpp
@@ -1,32 +1,42 @@
-#include <ros/ros.h> // ROS库
-#include <geometry_msgs/Twist.h> //Twist消息类型
-#include <autoware_msgs/VehicleCmd.h> //VehicleCmd消息类型
+#include <ros/ros.h>                      // ROS库
+#include <geometry_msgs/Twist.h>          //Twist消息类型
+#include <autoware_msgs/VehicleCmd.h>     //VehicleCmd消息类型
 #include <autoware_msgs/ControlCommand.h> //ControlCommand消息类型
 
-/* 
+#define USE_STAMPED 0
+
+/*
 TODO: 兼容不同的输出类型，如：Twist,TwistStamped等
 暂时选择Twist类型
 */
+
 class CmdVelToVehicleCmd
 {
 public:
     CmdVelToVehicleCmd()
     {
-        // 初始化订阅者，订阅/cmd_vel话题，消息类型为Twist
-        // sub = nh.subscribe("/cmd_vel", 1, &CmdVelToVehicleCmd::cmdVelCallback, this);
-        // 初始化发布者，发布到/vehicle_cmd话题,消息类型为VehicleCmd
+        // 初始化订阅者，订阅/vehicle_cmd话题，消息类型为TwistStamped
         vehicle_cmd_sub = nh.subscribe("/vehicle_cmd", 1, &CmdVelToVehicleCmd::vehicle_cmd_vel_callback, this);
-        // cmd_vel_pub = nh.advertise<geometry_msgs::TwistStamped>("/cmd_vel",1);
-        cmd_vel_without_stamped_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel",1);
+
+        // 初始化发布者，发布到/cmd_vel话题,消息类型为VehicleCmd
+        if (USE_STAMPED)
+        {
+            // 初始化订阅者，订阅/cmd_vel话题，消息类型为Twist
+            cmd_vel_pub = nh.advertise<geometry_msgs::TwistStamped>("/cmd_vel", 1);
+        }
+        else
+        {
+            cmd_vel_without_stamped_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+        }
     }
 
 private:
-    ros::NodeHandle nh; // ROS节点句柄
-    ros::Subscriber sub; // 订阅者句柄
+    ros::NodeHandle nh;              // ROS节点句柄
+    ros::Subscriber sub;             // 订阅者句柄
     ros::Subscriber vehicle_cmd_sub; ///< vehicle_cmd订阅者句柄
-    ros::Publisher pub; // 发布者句柄
+    ros::Publisher pub;              // 发布者句柄
 
-    // ros::Publisher cmd_vel_pub; ///< cmd_vel发布者句柄
+    ros::Publisher cmd_vel_pub;                 ///< cmd_vel发布者句柄
     ros::Publisher cmd_vel_without_stamped_pub; ///< cmd_vel发布者句柄
 
     void vehicle_cmd_vel_callback(const autoware_msgs::VehicleCmd::ConstPtr &msg)
@@ -41,7 +51,7 @@ private:
 
         // for gazebo
         ///< twist without stamped
-        cmd_vel_without_stamped.linear = msg->twist_cmd.twist.linear;    ///< 速度赋值
+        cmd_vel_without_stamped.linear = msg->twist_cmd.twist.linear;   ///< 速度赋值
         cmd_vel_without_stamped.angular = msg->twist_cmd.twist.angular; ///< 速度赋值
         cmd_vel_without_stamped_pub.publish(cmd_vel_without_stamped);
         // end
@@ -76,7 +86,7 @@ private:
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "vehiclecmd2twist_node"); // 初始化ROS节点
-    CmdVelToVehicleCmd cmd_vel_to_vehicle_cmd; // 创建CmdVelToVehicleCmd对象
-    ros::spin(); // 主循环，处理ROS消息
+    CmdVelToVehicleCmd cmd_vel_to_vehicle_cmd;      // 创建CmdVelToVehicleCmd对象
+    ros::spin();                                    // 主循环，处理ROS消息
     return 0;
 }
